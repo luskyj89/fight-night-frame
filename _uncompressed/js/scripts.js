a890f3bea6a378cc54caec707803f3d5f8ada8d3
John Lusky <lusky@1trickpony.com>
2015-04-30 02:11:02 -0400

    Initial commit

--- _uncompressed/js/scripts.js
+++ _uncompressed/js/scripts.js
@@ -1,28 +1,185 @@
+function badgeRestart() {
+    $(".logo").delay(4000)
+        .animate(
+        {
+            transform: 360,
+            right: "-500px",
+            opacity: 0
+        },
+        {
+            duration: 2000,
+            step: function(now, fx) {
+                if (fx.prop == "transform") {
+                    $(this).css({
+                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                    })
+                }
+            },
+            complete: function() {
+                followAnimation();
+            }
+        });
+}
 
-
-// videoBG
-function videoStarter() {
-    $('.space').videoBG({
-        mp4:'video/lightning.mp4',
-        ogv:'video/shutterstock_v2529842.ogv',
-        webm:'video/lightning.webm',
-        poster:'video/shutterstock_v2529842.jpg',
-        scale:true,
-        zIndex:0
+function badgeInEnd() {
+$(".logo")
+    .animate(
+    {
+        transform: 1080,
+        right: "1px",
+        opacity: 1
+    },
+    {
+        duration: 2000,
+        step: function(now, fx) {
+            if (fx.prop == "transform") {
+                if (now > 360) {  // rotate and fade in when image already rotated 360deg
+                    $(this).css({
+                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                    })
+                }
+            }
+        },
+        complete: function() {
+            // How long the animation takes to restart
+            setTimeout(badgeRestart, 600000);
+        }
     });
+
+}
+
+function followLeave() {
+    $(".follow").delay(10000)
+        .animate(
+        {
+            transform: 360,
+            right: "-500px",
+            opacity: 0
+        },
+        {
+            duration: 2000,
+            step: function(now, fx) {
+                if (fx.prop == "transform") {
+                    $(this).css({
+                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                    })
+                }
+            },
+            complete: function() {
+                badgeInEnd();
+            }
+        });
+};
+
+function badgeLeave() {
+    $(".logo").delay(4000)
+        .animate(
+        {
+            transform: 360,
+            right: "-500px",
+            opacity: 0
+        },
+        {
+            duration: 2000,
+            step: function(now, fx) {
+                if (fx.prop == "transform") {
+                    $(this).css({
+                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                    })
+                }
+            },
+            complete: function() {
+                followAnimation();
+            }
+        });
+};
+
+function followAnimation() {
+    $(".follow")
+        .animate(
+        {
+            transform: 1080,
+            right: "1px",
+            opacity: 1
+        },
+        {
+            duration: 2000,
+            step: function(now, fx) {
+                if (fx.prop == "transform") {
+                    if (now > 360) {  // rotate and fade in when image already rotated 360deg
+                        $(this).css({
+                            '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                            '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                            '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                            '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                            'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                        })
+                    }
+                }
+            },
+            complete: function() {
+                followLeave();
+            }
+        });
+}
+
+function badgeIn() {
+$(".logo")
+    .animate(
+    {
+        transform: 1080,
+        right: "1px",
+        opacity: 1
+    },
+    {
+        duration: 2000,
+        step: function(now, fx) {
+            if (fx.prop == "transform") {
+                if (now > 360) {  // rotate and fade in when image already rotated 360deg
+                    $(this).css({
+                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                    })
+                }
+            }
+        },
+        complete: function() {
+            badgeLeave();
+        }
+    });
+
 }
 
 function init() {
 
-    $('.countdown').countdown('01/31/2015 21:00:00', function(event) {
-          var $this = $(this).html(event.strftime(''
-            + '<span>%d</span> days '
-            + '<span>%H</span> hr '
-            + '<span>%M</span> min '
-            + '<span>%S</span> sec'));
-        });
+    // $( ".logo" ).animate({
+    //     opacity: 1,
+    //     right: "1px"
+    // }, 2000, function() {
+    //     // Animation complete.
+    //
+    // });
 
-    videoStarter();
+    badgeIn();
 
 }
 
