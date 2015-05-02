efd18f5d457faa9a8e62344ccf374eb57b78e411
John Lusky <info@johnlusky.com>
2015-05-01 22:25:55 -0400

    no rotate browser too old to handle

--- _uncompressed/js/scripts.js
+++ _uncompressed/js/scripts.js
@@ -11,11 +11,11 @@ function badgeRestart() {
             step: function(now, fx) {
                 if (fx.prop == "transform") {
                     $(this).css({
-                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
-                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
-                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
-                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
-                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                        // '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        // '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        // '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        // '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        // 'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
                     })
                 }
             },
@@ -39,11 +39,11 @@ $(".logo")
             if (fx.prop == "transform") {
                 if (now > 360) {  // rotate and fade in when image already rotated 360deg
                     $(this).css({
-                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
-                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
-                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
-                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
-                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                        // '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        // '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        // '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        // '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        // 'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
                     })
                 }
             }
@@ -69,11 +69,11 @@ function followLeave() {
             step: function(now, fx) {
                 if (fx.prop == "transform") {
                     $(this).css({
-                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
-                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
-                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
-                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
-                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                        // '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        // '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        // '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        // '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        // 'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
                     })
                 }
             },
@@ -96,11 +96,11 @@ function badgeLeave() {
             step: function(now, fx) {
                 if (fx.prop == "transform") {
                     $(this).css({
-                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
-                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
-                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
-                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
-                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                        // '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        // '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        // '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        // '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        // 'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
                     })
                 }
             },
@@ -124,11 +124,11 @@ function followAnimation() {
                 if (fx.prop == "transform") {
                     if (now > 360) {  // rotate and fade in when image already rotated 360deg
                         $(this).css({
-                            '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
-                            '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
-                            '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
-                            '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
-                            'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                            // '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                            // '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                            // '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                            // '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                            // 'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
                         })
                     }
                 }
@@ -153,11 +153,11 @@ $(".logo")
             if (fx.prop == "transform") {
                 if (now > 360) {  // rotate and fade in when image already rotated 360deg
                     $(this).css({
-                        '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
-                        '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
-                        '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
-                        '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
-                        'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
+                        // '-webkit-transform': 'rotate(' + now + 'deg)',  //Safari 3.1+, Chrome
+                        // '-moz-transform': 'rotate(' + now + 'deg)',     //Firefox 3.5-15
+                        // '-ms-transform': 'rotate(' + now + 'deg)',      //IE9+
+                        // '-o-transform': 'rotate(' + now + 'deg)',       //Opera 10.5-12.00
+                        // 'transform': 'rotate(' + now + 'deg)',          //Firefox 16+, Opera 12.50+
                     })
                 }
             }
